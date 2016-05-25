c59489c18370eb02312864c502685375054419c7
Matthew Davis <mdavis0816@gmail.com>
2016-05-24 22:28:00 -0700

    take in store. emit socket events

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 import Server from 'socket.io'
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090)
-  console.log('Server listening...')
+  console.log('server listening...')
+
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  )
+  console.log('current state:\n')
+  console.log(store.getState().toJS())
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS())
+    socket.on('action', store.dispatch.bind(store))
+  })
 }
